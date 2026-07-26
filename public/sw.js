07596fb0eb00a8e43fbc3e642ead77b6732ea309
Kiwimii <64694181+Kiwimii@users.noreply.github.com>
2026-07-26 23:10:50 +0200

    Bump Next cache for realism release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tales-adria-next-s55';
+const CACHE_NAME = 'tales-adria-next-s56';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
