cdc49d55f29105f7db2b4d83fae4612c5836416e
Kiwimii <64694181+Kiwimii@users.noreply.github.com>
2026-07-27 07:53:02 +0200

    Sprint 76: mount single optimized application shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import { GlobalGameControls } from './components/GlobalGameControls';
+import OptimizedApp from './OptimizedApp';
 import './styles.css';
 
 const root = document.getElementById('root');
@@ -12,8 +11,7 @@ if (!root) {
 
 createRoot(root).render(
   <StrictMode>
-    <App />
-    <GlobalGameControls />
+    <OptimizedApp />
   </StrictMode>,
 );
 
