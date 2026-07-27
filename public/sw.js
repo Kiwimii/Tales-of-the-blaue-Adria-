d3c3e17db575985fc93ac850b94b9b152ef33495
Kiwimii <64694181+Kiwimii@users.noreply.github.com>
2026-07-27 23:18:29 +0200

    Version Sprint 87 PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tales-adria-next-s86';
+const CACHE_NAME = 'tales-adria-next-s87';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
