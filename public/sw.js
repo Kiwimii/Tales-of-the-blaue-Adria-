58384a3c03b0d6c90508443cf9713f046bbe859f
Kiwimii <64694181+Kiwimii@users.noreply.github.com>
2026-07-27 15:58:44 +0200

    Bump Next cache to Sprint 82

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tales-adria-next-s81';
+const CACHE_NAME = 'tales-adria-next-s82';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
