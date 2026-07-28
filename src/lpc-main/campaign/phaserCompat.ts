963db7536b080cdfca7237a2367d9e148e7abeee
Kiwimii <64694181+Kiwimii@users.noreply.github.com>
2026-07-29 01:08:48 +0200

    Fix Phaser 4 campaign compatibility at runtime

---
+++ src/lpc-main/campaign/phaserCompat.ts
@@ -0,0 +1,28 @@
+import Phaser from 'phaser';
+
+type ExtendedGraphics = Phaser.GameObjects.Graphics & {
+  quadraticBezierTo: (controlX: number, controlY: number, x: number, y: number) => ExtendedGraphics;
+};
+
+const graphicsPrototype = Phaser.GameObjects.Graphics.prototype as unknown as Partial<ExtendedGraphics>;
+if (typeof graphicsPrototype.quadraticBezierTo !== 'function') {
+  Object.defineProperty(graphicsPrototype, 'quadraticBezierTo', {
+    configurable: true,
+    value(this: Phaser.GameObjects.Graphics, _controlX: number, _controlY: number, x: number, y: number): ExtendedGraphics {
+      this.lineTo(x, y);
+      return this as ExtendedGraphics;
+    },
+  });
+}
+
+const cameraPrototype = Phaser.Cameras.Scene2D.Camera.prototype as Phaser.Cameras.Scene2D.Camera & { rotation?: number };
+if (!Object.prototype.hasOwnProperty.call(cameraPrototype, 'rotation')) {
+  Object.defineProperty(cameraPrototype, 'rotation', { configurable: true, writable: true, value: 0 });
+}
+
+const geom = Phaser.Geom as typeof Phaser.Geom & { Point?: new (x: number, y: number) => { x: number; y: number } };
+if (!geom.Point) {
+  geom.Point = class Point {
+    constructor(public x: number, public y: number) {}
+  };
+}
